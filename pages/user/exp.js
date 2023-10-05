6c6308142d7aed05f2465bcf3a4c01b92d44a788
lixxth <lizbeth78765@gmail.com>
2023-10-05 04:36:51 -0600

    parte 3 con flex

--- pages/user/exp.js
+++ pages/user/exp.js
@@ -6,6 +6,8 @@ export default function Exp() {
   return (
     <>
       <SideBar>
+
+
       <div className="relative flex flex-col items-center justify-center bg-[url('/imgExp/portadaExp.jpg')] no-repeat center center bg-cover w-full h-[500px]">
     {/* <img src="/imgExp/portadaExp.jpg" alt="" class="w-full h-96" /> */}
     <div class="relative flex items-center justify-center w-52 h-72 bg-[#088022] rounded-3xl z-10">
@@ -13,27 +15,27 @@ export default function Exp() {
     <div class="absolute flex items-center justify-center w-72 h-52 bg-[#ffffff] rounded-3xl z-20">
         {/* Contenido blanco */}
         <p class= "absolute bottom-28 left-4 text-3xl text-black px-2 py-1 text-center pr-2 font-bold ">EXPERIENCIAS DE VIAJE UNICAS </p>
-        
+        <button
+         class="relative none bottom-0 top-10 pl-40 rounded-lg bg-gradient-to-tr from-[#193300] to-[#33ff99] py-6  px-4  font-sans text-[4px]  font-bold uppercase text-white shadow-md shadow-green-950/40 transition-all hover:shadow-lg hover:shadow-green-950/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+          <div className="absolute flex flex-col items-center justify-center right-6 "></div> 
+          <div
+         class="absolute none bottom-0 left-10  rounded-lg bg-gradient-to-tr from-[#193300] to-[#33ff99] py-[14px]  px-14  font-sans text-[4px]  font-bold uppercase text-white shadow-md shadow-green-950/40 transition-all hover:shadow-lg hover:shadow-green-950/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+          <div className="relative flex flex-col items-center justify-center  center bg-cover w-6 h-[20px] object-cover "></div> 
+         </div>
+         </button>
     </div>
 </div>
 
 
-        <div className="relative">
-          <div class="w-full h-[600px] bg-white">
-            <a
-              href="/tu-enlace3"
-              class="relative top-8 left-14 w-52 h-60 rounded-xl bg-red-100 block no-underline"
-            >
-              <img
-                src="/imgExp/sanCarlos.jpg"
-                alt=""
-                class="w-full h-44 sm:h-60 rounded-xl"
-              />
-              <p class="absolute bottom-8 left-3 text-sm text-white px-2 py-1 font-bold">
-                <div id="linea" class="w-32 h-1 bg-white"></div>
-                MIRADOR PANORÁMICO SAN CARLOS
-              </p>
-            </a>
+<div className="relative flex flex-col items-center justify-center bg-[url('/imgExp/portadaExp.jpg')] no-repeat center center bg-cover w-full h-[500px]">
+  <div className="w-full h-[600px] bg-white">
+    <a href="/tu-enlace3" className="relative top-8 left-14 w-52 h-60 rounded-xl bg-red-100 block no-underline">
+      <img src="/imgExp/sanCarlos.jpg" alt="" className="w-full h-44 sm:h-60 rounded-xl" />
+      <p className="absolute bottom-8 left-3 text-sm text-white px-2 py-1 font-bold">
+        <div className="w-32 h-1 bg-white"></div>
+        MIRADOR PANORÁMICO SAN CARLOS
+      </p>
+    </a>
 
             <a
               href="/tu-enlace4"
@@ -112,15 +114,35 @@ export default function Exp() {
           </div>
         </div>
 
-        <div className="relative">
+        <div className="relative flex flex-col items-center justify-center">
           <div class="w-full h-[600px] bg-[#088022]">
+          <p class="absolute bottom-72 left-16 text-1xl text-white px-2 py-1 font-bold">
+          MUSEO SONORA REVOLUCION
+                <div id="linea" class="w-60 h-1 bg-white"></div>
+              </p>
+              <p class="absolute bottom-24 left-[330px] text- text-white px-10 py-6 ">
+              Este museo te invita a explorar la apasionante historia de la Revolución Mexicana. A través de una valiosa colección de piezas históricas resguardadas por descendientes del General Álvaro Obregón, podrás sumergirte en este período crucial de la historia de México.
+
+Cada objeto en exhibición cuenta una historia única, desde armas de la época hasta documentos originales y objetos personales de figuras destacadas. Estas piezas te llevarán al contexto histórico de la Revolución, permitiéndote comprender las luchas y los logros de quienes participaron en ella.
+
+Descubre una experiencia enriquecedora que te conectará con el pasado y te permitirá apreciar la rica herencia cultural de la Revolución Mexicana a través de estas valiosas reliquias.
+              </p>
             <img
-              src="/imgExp/portadaExp.jpg"
+              src="/imgExp/museo.webp"
               alt=""
-              class=" w-[700px] h-96 left-20"
+              class=" w-[745px] h-[580px] pt-6 pl-7"
             />
           </div>
         </div>
+ 
+        <div className="relative flex flex-col items-center justify-center">
+          <div class="w-full h-[600px] bg-[#ffffff]">
+          <div class="w-80 h-96 bottom  bg-[#CCFFE5] ">
+          </div>
+          </div>
+        </div>
+
+
       </SideBar>
     </>
   );
