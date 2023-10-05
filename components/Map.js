b98c67128e1a0e7922476444ed4581065af1213f
KiraBelak <kalebchivas@hotmail.com>
2023-10-04 18:13:47 -0600

    map screen

--- components/Map.js
+++ components/Map.js
@@ -105,7 +105,7 @@ useEffect(() => {
   };
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '600px' }}>
+    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
     <div id="map" style={{ width: '100%', height: '100%' }} />
     <button
       onClick={resetMapView}
