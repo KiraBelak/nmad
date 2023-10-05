f83e192b71ce826983ef205991325633b90766b7
Sergio Ivan Pinon Pena <sergioivan.pinon@gmail.com>
2023-10-04 23:38:47 -0600

    Creacion de Selector de Leguage

---
+++ components/LenguageSelector.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LenguageSelector(){
+    return (
+        <select>
+            <option value="es">Español</option>
+            <option value="en">Ingles</option>
+            <option value="fr">Frances</option>
+        </select>
+    )
+}
