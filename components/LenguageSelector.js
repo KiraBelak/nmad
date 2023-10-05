54ce564eee9fe03cfea67f78fa0bc9f0aaac1756
Sergio Ivan Pinon Pena <sergioivan.pinon@gmail.com>
2023-10-05 00:48:41 -0600

    Se agrego la parte para que se cambie de idioma

--- components/LenguageSelector.js
+++ components/LenguageSelector.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useRouter } from "next/router"
 
 export default function LenguageSelector(){
+    const router = useRouter();
+    console.log(router);
+
+        // e de evento
+        const changeLang = (e) => {
+            router.push(router.pathname, router.pathname, {
+                locale: e.target.value,
+            });
+        }
     return (
-        <select>
+        <select onChange={changeLang}>
             <option value="es">Español</option>
             <option value="en">Ingles</option>
             <option value="fr">Frances</option>
         </select>
-    )
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import {
   ServerIcon,
 } from '@heroicons/react/20/solid'
 import LoginBtn from '@/components/LoginBtn'
+import LenguageSelector from '@/components/LenguageSelector'
 
 const navigation = [
   // { name: 'Product', href: '#' },
@@ -234,6 +235,7 @@ export default function Example() {
                 <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
                 Viaja al precio mas bajo y haz amigos en cada destino
                 </h1>
+                <LenguageSelector />
                 <p className="mt-6 text-lg leading-8 text-gray-300">
                 Transforma tus viajes en experiencias inolvidables al conectar nómadas digitales con locales: evita impuestos y forja amistades por todo el mundo
                 </p>
