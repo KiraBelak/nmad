ff22099ad9a4fd6425d958571db5fb3bcf75a615
Sergio Ivan Pinon Pena <sergioivan.pinon@gmail.com>
2023-10-05 01:33:54 -0600

    Se borraron log que solo eran de prueba

--- components/LenguageSelector.js
+++ components/LenguageSelector.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router"
 
 export default function LenguageSelector(){
     const router = useRouter();
-    console.log(router);
+    
 
         // e de evento
         const changeLang = (e) => {
