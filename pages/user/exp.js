c8064b72505458ddc1c6dd9e39b1040ee3c6b426
lixxth <lizbeth78765@gmail.com>
2023-10-05 03:13:57 -0600

    cambios a flex de exp

--- pages/user/exp.js
+++ pages/user/exp.js
@@ -1,101 +1,127 @@
 import SideBar from "@/components/layouts/Sidebar";
 
-
-
 export default function Exp() {
-   //logica js aqui
-   
-    return (
-        <>
-        <SideBar>
+  //logica js aqui
+
+  return (
+    <>
+      <SideBar>
+      <div className="relative flex flex-col items-center justify-center bg-[url('/imgExp/portadaExp.jpg')] no-repeat center center bg-cover w-full h-[500px]">
+    {/* <img src="/imgExp/portadaExp.jpg" alt="" class="w-full h-96" /> */}
+    <div class="relative flex items-center justify-center w-52 h-72 bg-[#088022] rounded-3xl z-10">
+    </div>
+    <div class="absolute flex items-center justify-center w-72 h-52 bg-[#ffffff] rounded-3xl z-20">
+        {/* Contenido blanco */}
+        <p class= "absolute bottom-28 left-4 text-3xl text-black px-2 py-1 text-center pr-2 font-bold ">EXPERIENCIAS DE VIAJE UNICAS </p>
+        
+    </div>
+</div>
 
 
-      
-    
         <div className="relative">
-        <img src="/imgExp/portadaExp.jpg" alt="" class="w-full h-96" />
-        <div class="absolute bottom-14 left-[280PX]  w-52 h-72  bg-[#088022] rounded-3xl"></div>   
-        <div class="absolute bottom-24 left-56  w-80 h-52 bg-white rounded-3xl">
-        <p class="absolute top-4 left-4 right-0 bottom-0 pl-5 text-black text-3xl font-semibold">EXPERIENCIAS DE</p>
-        <p class="absolute top-14 left-4 right-0 bottom-0 pl-9 text-black text-3xl font-semibold">VIAJES UNICAS</p>
-        <div
-         class="absolute none top-28  left-16 rounded-lg bg-gradient-to-tr from-green-200 to-green-500 py-4 px-24  font-sans text-[4px] font-bold uppercase text-white shadow-md shadow-lime-950/20 transition-all hover:shadow-lg hover:shadow-lime-950/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-         <button
-         class="absolute none top-0 left-0 rounded-lg bg-transparente py-4  px-9 font-sans text-[4px] font-bold uppercas  ">
-         <img src="/imgExp/busqueda.png" alt="busqueda" class="w-6 h-6 absolute top-1 left-2" />
-         </button>
-         </div>
-        </div> 
-    </div> 
+          <div class="w-full h-[600px] bg-white">
+            <a
+              href="/tu-enlace3"
+              class="relative top-8 left-14 w-52 h-60 rounded-xl bg-red-100 block no-underline"
+            >
+              <img
+                src="/imgExp/sanCarlos.jpg"
+                alt=""
+                class="w-full h-44 sm:h-60 rounded-xl"
+              />
+              <p class="absolute bottom-8 left-3 text-sm text-white px-2 py-1 font-bold">
+                <div id="linea" class="w-32 h-1 bg-white"></div>
+                MIRADOR PANORÁMICO SAN CARLOS
+              </p>
+            </a>
 
-    <div className="relative">
-    <div class="w-full h-[600px] bg-white">  
-    <div class="relative top-8 left-14 w-52 h-60 rounded-xl  bg-red-100">
-    <img src="/imgExp/sanCarlos.jpg" alt="" class="w-full h-44 sm:h-60 rounded-xl" />
-    <p class="absolute bottom-8 left-3 text-sm  text-white px-2 py-1 font-bold">
-    <div id="linea" class="w-32 h-1 bg-white"></div>
-     MIRADOR PARANOMICO SAN CARLOS</p>
-    </div>
+            <a
+              href="/tu-enlace4"
+              class="relative bottom-52 left-72 w-52 h-60 rounded-xl bg-red-100 block no-underline"
+            >
+              <img
+                src="/imgExp/Laguna.jpg"
+                alt=""
+                class="w-full h-44 sm:h-60 rounded-xl"
+              />
+              <p class="absolute bottom-12 left-3 text-sm text-white px-2 py-1 font-bold">
+                <div id="linea" class="w-32 h-1 bg-white"></div>
+                LA LAGUNA DE NAINARI
+              </p>
+            </a>
 
-    <div class="relative bottom-52 left-72 w-52 h-60 rounded-xl   bg-red-100">
-    <img src="/imgExp/Laguna.jpg" alt="" class="w-full h-44 sm:h-60 rounded-xl" />
-    <p class="absolute bottom-12 left-3 text-sm  text-white px-2 py-1 font-bold">
-    <div id="linea" class="w-32 h-1  bg-white"></div>
-    LA LAGUNA DE NAINARI</p>   
-    </div>
+            <a
+              href="/tu-enlace5"
+              class="relative bottom-[446px] left-2/3 w-52 h-60 rounded-xl bg-red-100 block no-underline"
+            >
+              <img
+                src="/imgExp/coco.jpg"
+                alt=""
+                class="w-full h-44 sm:h-60 rounded-xl"
+              />
+              <p class="absolute bottom-12 left-3 text-sm text-white px-2 py-1 font-bold">
+                <div id="linea" class="w-32 h-1 bg-white"></div>
+                COCORIT
+              </p>
+            </a>
 
-    <div class="relative bottom-[446px] left-2/3 w-52 h-60 rounded-xl   bg-red-100">
-    <img src="/imgExp/coco.jpg" alt="" class="w-full h-44 sm:h-60 rounded-xl" />
-    <p class="absolute bottom-12 left-3 text-sm  text-white px-2 py-1 font-bold">
-    <div id="linea" class="w-32 h-1  bg-white"></div>
-    COCORIT</p>  
-    </div>
+            <a
+              href="/tu-enlace"
+              class="relative bottom-[412px] left-2/3 w-52 h-60 rounded-xl bg-red-100 block no-underline"
+            >
+              <img
+                src="/imgExp/ECO.jpg"
+                alt=""
+                class="w-full h-44 sm:h-60 rounded-xl"
+              />
+              <p class="absolute bottom-12 left-3 text-sm text-white px-2 py-1 font-bold">
+                <div id="linea" class="w-32 h-1 bg-white"></div>
+                ECO CAMP INN
+              </p>
+            </a>
 
-    <div class="relative bottom-[412px] left-2/3 w-52 h-60 rounded-xl  bg-red-100">
-    <img src="/imgExp/ECO.jpg" alt="" class="w-full h-44 sm:h-60 rounded-xl" />
-    <p class="absolute bottom-12 left-3 text-sm  text-white px-2 py-1 font-bold">
-    <div id="linea" class="w-32 h-1  bg-white"></div>
-    ECO CAMP INN</p>  
-    </div>
+            <a
+              href="/tu-enlace1"
+              class="relative bottom-[655px] left-72 w-52 h-60 rounded-xl bg-red-100 block no-underline"
+            >
+              <img
+                src="/imgExp/ESTADIO2.jpg"
+                alt=""
+                class="w-full h-44 sm:h-60 rounded-xl"
+              />
+              <p class="absolute bottom-12 left-3 text-sm text-white px-2 py-1 font-bold">
+                <div id="linea" class="w-32 h-1 bg-white"></div>
+                ESTADIO DE LOS YAQUIS
+              </p>
+            </a>
 
-    <div class="relative bottom-[655px] left-72 w-52 h-60 rounded-xl  bg-red-100">
-    <img src="/imgExp/ESTADIO2.jpg" alt="" class="w-full h-44 sm:h-60 rounded-xl" />
-    <p class="absolute bottom-12 left-3 text-sm  text-white px-2 py-1 font-bold">
-    <div id="linea" class="w-32 h-1  bg-white"></div>
-    ESTADIO DE LOS YAQUIS</p>  
-    </div>
+            <a
+              href="/tu-enlace2"
+              class="relative bottom-[895px] left-14 w-52 h-60 rounded-xl bg-red-100 block no-underline"
+            >
+              <img
+                src="/imgExp/GALICIA.jpg"
+                alt=""
+                class="w-full h-44 sm:h-60 rounded-xl"
+              />
+              <p class="absolute bottom-12 left-3 text-sm text-white px-2 py-1 font-bold">
+                <div id="linea" class="w-32 h-1 bg-white"></div>
+                GALICIA OUTDOORS
+              </p>
+            </a>
+          </div>
+        </div>
 
-    <div class="relative bottom-[895px] left-14 w-52 h-60 rounded-xl  bg-red-100">
-    <img src="/imgExp/GALICIA.jpg" alt="" class="w-full h-44 sm:h-60 rounded-xl" />
-    <p class="absolute bottom-12 left-3 text-sm  text-white px-2 py-1 font-bold">
-    <div id="linea" class="w-32 h-1  bg-white"></div>
-    GALICIA OUTDOORS</p>
-    </div>
-
-    </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </SideBar>
-
-        </>
-    )
+        <div className="relative">
+          <div class="w-full h-[600px] bg-[#088022]">
+            <img
+              src="/imgExp/portadaExp.jpg"
+              alt=""
+              class=" w-[700px] h-96 left-20"
+            />
+          </div>
+        </div>
+      </SideBar>
+    </>
+  );
 }
