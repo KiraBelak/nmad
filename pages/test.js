18add186bbe699cffe36949a4742a57bb9fe3705
KiraBelak <kalebchivas@hotmail.com>
2023-10-05 03:53:12 -0600

    upload to image

--- pages/test.js
+++ pages/test.js
@@ -1,14 +1,22 @@
-<script src="https://upload-widget.cloudinary.com/global/all.js" type="text/javascript">
-</script>
 import axios from "axios";
 import { useState } from "react";
 
 export default function Fotos() {
-  const [file, setFile] = useState(null)
-  const sube = async () => {
+  const [file, setFile] = useState(null);
+  const sube = async (e) => {
     console.log("click");
     const datos = "";
-    console.log(file)
+    console.log(file);
+    const formData = new FormData();
+    formData.append("file", file);
+    // formData.append("folder", "images");
+    formData.append("upload_preset", "s0uookdo");
+    const res = await axios.post(
+      `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+      formData
+    );
+    console.log(res);
+
 
     // const { data: cloudinaryData } = await axios.post(
     //   `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
@@ -24,22 +32,25 @@ export default function Fotos() {
 
   return (
     <>
-      <input type="file" onChange={async(e)=>(await setFile(e.target.value))} />
+      <input
+        type="file"
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+        }
+        }
+      />
 
       <br />
-      
-    <button className="cloudinary-button" onClick={sube}>
-          subir
+
+      <button className="cloudinary-button" onClick={sube}>
+        subir
       </button>
 
       <p></p>
       <div id="divuploadedimage"></div>
       <p></p>
-      
-    <img  
-      id="uploadedimage" 
-      src="api/foto1.jpg">
-    </img>
+
+      <img id="uploadedimage" src="api/foto1.jpg"></img>
     </>
   );
 }
