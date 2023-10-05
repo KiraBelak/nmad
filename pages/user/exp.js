598a4641fd6ab0b585b6b21a5109c45d6b33ee91
lixxth <lizbeth78765@gmail.com>
2023-10-04 21:32:15 -0600

    responsividad de la pagina

---
+++ pages/user/exp.js
@@ -0,0 +1,17 @@
+import SideBar from "@/components/layouts/Sidebar";
+
+
+
+export default function Exp() {
+   //logica js aqui
+   
+    return (
+        <>
+        <SideBar>
+Hola
+
+        </SideBar>
+
+        </>
+    )
+}
---
+++ pages/user/nomadas.js
@@ -0,0 +1,118 @@
+import SideBar from "@/components/layouts/Sidebar";
+
+
+
+export default function Nomadas() {
+   //logica js aqui 
+   
+    return (
+        <>
+        <SideBar>
+
+    <div>
+        <div className="relative">
+        <img src="/imgNomadas/tequila.jpg" alt="" class="w-full" />
+        <img src="/imgNomadas/edgar.jpg" alt="" class=" w-28 h-28 sm: w-52 sm:h-52  absolute top-10 sm:top-28 left-52 sm:left-2/3 lg:left-[440px] rounded-md" />
+        <p class="absolute bottom-24 sm:bottom-52 lg:bottom-38 left-5 sm:left-12 lg:left-8 text-2xl sm:text-5xl lg:text-5xl text-black hover:text-red-400  px-2 py-1">Tequila Jalisco</p>    
+        <p class="absolute bottom-4 sm:bottom-10 lg:bottom-6 left-52 sm:left-2/3 lg:left-[440px] text-base sm:text-3xl text-orange-200 bg-gradient-to-r from-orange-600  px-2 py-1 font-bold">Edgar Robles</p>   
+         <p class= "absolute bottom-12 sm:bottom-24 lg:bottom-14 left-3 text-[7px] sm:text-base text-white  px-2 py-1 pr-32 sm:pr-64 ">Descubre la esencia de México en Tequila, Jalisco,
+          donde la cultura del tequila cobra vida entre majestuosos campos de agave azul.
+           ¡Sumérgete en la destilación artesanal y degusta esta icónica bebida en un entorno pintoresco conmigo!.</p>
+          
+    </div>
+    
+
+    </div>
+    <div className="relative flex"> 
+  <div class="w-full h-48 bg-amber-50"></div>
+  <p class="absolute top-8 sm:top-10 left-8 right-0 bottom-0   text-red-600 text-sm sm:text-1xl font-semibold">Edgar Robles</p> 
+  <p class="absolute top-14 sm:top-16 pr-40 sm:pr-96 pl-7 text-gray-600 text-[8px] sm:text-xs" > 
+  Soy un nmado de jalisco,apasionado por
+  la aventura, la adrenalina y
+  los recorridos que involucren fiesta y 
+  buenos amigos, permiteme ayudarte a 
+  descubrir jalisco.
+  mi mision *Jalisco es mi hogar, y mi pasión
+  es compartir su belleza y cultura con el mundo* </p> 
+
+  <p class="absolute top-14 sm:top-16 lg:top-8 left-40 sm:left-96 pr-28 sm:pr-16 pl-7 text-gray-600 text-[8px] sm:text-xs" > 
+  Soy un nmado de jalisco,apasionado por
+  la aventura, la adrenalina y 
+  los recorridos que involucren fiesta y 
+  buenos amigos, permiteme ayudarte a 
+  descubrir jalisco.
+  mi mision *Jalisco es mi hogar, y mi pasión
+  es compartir su belleza y cultura con el mundo* </p>
+
+   </div>
+   <div className="relative flex"> 
+    <div class="w-full h-60 bg-orange-400"></div>
+    <p class="absolute top-4 sm:top-4 left-24 sm:left-64 right-0 bottom-0   text-gray-500 text-sm sm:text-xl font-semibold">Porque viajar conmigo?</p> 
+    <p class="absolute top-12 sm:top-16 left-6 sm:left-32 lg:left right-0 bottom-0  text-black text-[7.5px] sm:text-xs font-semibold">Ademas de mi amistad y de mi basto conocimiento en los lugares de jalisco te ofrezco:</p> 
+    <img src="/imgNomadas/ViajeSeguro.jpg" alt="viaje seguro" class=" w-8 sm:w-12 h-8 sm:h-12 absolute top-20 sm:top-24 left-10 sm:left-28 rounded-full " />
+    <img src="/imgNomadas/des.png" alt="descubre" class=" w-8 sm:w-12 h-8 sm:h-12 absolute top-16 sm:top-24 left-36 sm:left-[350px] rounded-full " />
+    <img src="/imgNomadas/multiple.jpg" alt="descubre" class=" w-8 sm:w-12 h-8 sm:h-12 absolute top-16 sm:top-24 left-64 sm:left-[585px] rounded-full " />
+    <p class="absolute top-28 sm:top-40 left-8 sm:left-24 right-0 bottom-0   text-black text-[7.5px] sm:text-[10px] font-semibold">VIAJE SEGURO</p>
+    <p class="absolute top-32 sm:top-[180px] left-4 sm:left-12 right-0 bottom-0 pr-60 sm:pr-[500px]  text-gray-500 text-[5px] sm:text-[7px] font-semibold text-center">Soy una persona de confianza,mis resenas pueden ayudarte a darte una idea, ademas no hay tarifas elevadas por ser turista, a un amigo no le cobraria de mas</p>
+    <p class="absolute top-28 sm:top-[160px] left-36 sm:left-[350px] right-0 bottom-0   text-black text-[7.5px] sm:text-[10px] font-semibold">DESCUBRE </p>
+    <p class="absolute top-32 sm:top-[180px] left-32 sm:left-[270px] right-0 bottom-0 pr-36 sm:pr-[250px] text-gray-500 text-[5px] sm:text-[7px] font-semibold text-center">Soy una persona de confianza,mis resenas pueden ayudarte a darte una idea, ademas no hay tarifas elevadas por ser turista, a un amigo no le cobraria de mas</p>
+    <p class="absolute top-28 sm:top-[160px] left-60 sm:left-[560px] right-0 bottom-0   text-black text-[7.5px] sm:text-[10px] font-semibold">MULTIPLE DESTINOS </p>
+    <p class="absolute top-32 sm:top-[180px] left-56 sm:left-[520px] right-0 bottom-0 pr-7 text-gray-500 text-[5px] sm:text-[7px] font-semibold text-center">Soy una persona que lleva muchos anos viviendo en jalisco,y puedo darte multiples experiencias dependiendo el destinos que te gustaria conocer.</p>
+    </div>
+
+    <div className="relative">
+    <img src="/imgNomadas/resenas.webp" alt="" class="w-full h-44 sm:h-60" />
+    <p class="absolute bottom-24 sm:bottom-32 left-3 text-sm sm:text-3xl text-black hover:text-red-400 px-2 py-1 font-bold">MIS AMIGOS HABLAN POR MI</p>       
+         <p class= "absolute bottom-14 sm:bottom-10 left-0  sm:left-3 text-[8px] sm:text-lg text-white px-2 py-1 text-center pr-32 sm:pr-60">No me creas a mi, puedes checar las reseñas de mis 
+         amigos que me han venido a visitar y hemos dado una vueltas por la ciudad juntos.</p>
+         <button
+         class="absolute none top-20 sm:top-28 left-60 sm:left-3/4 rounded-lg bg-gradient-to-tr from-red-500 to-orange-500 py-2 sm:py-4 px-4 sm:px-8 font-sans text-[8px] sm:text-[10px] font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+         Reseñas
+         </button>
+    </div>
+      
+
+    <div className="relative">
+    <div class="w-full h-48 sm:h-72 bg-amber-50">
+    <div class="absolute bottom-6  left-60 sm:left-[490px] w-20 sm:w-48 h-32 sm:h-60 bg-amber-100">
+    <p class= "absolute bottom-12 sm:bottom-20 left-4 sm:left-14 text-[5px] sm:text-[10px] text-orange-500 px-2 py-1 text-center pr-2 font-bold ">CENTRO GDL</p>
+    <img src="/imgNomadas/centroGDL.jpg" alt="" class=" w-16 sm:w-44 h-14 sm:h-32 absolute top-2 left-2" />
+    <p class= "absolute bottom-4 sm:bottom-8 left-0 text-[5px] sm:text-[10px] text-orange-500 px-2 py-1 text-center pr-2 ">Tomemos un recorrido por el centro de guadalaja y aprendamos de su arquitectura y comida tipica.</p>
+    <button
+         class="absolute none top-28 sm:top-52 left-6 sm:left-[60px]  rounded-lg bg-gradient-to-tr from-red-500 to-orange-500 py-1 sm:py-2 px-2 sm:px-4 font-sans text-[4px] sm:text-[7px] font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+         Mas info
+         </button>
+    </div>
+    <div class="absolute bottom-6 left-32 sm:left-[260px] w-20 sm:w-48 h-32 sm:h-60  bg-amber-100">
+    <p class= "absolute bottom-12 sm:bottom-20 left-2 sm:left-[38px] text-[5px] sm:text-[10px] text-orange-500 px-2 py-1 text-center pr-2 font-bold ">PUERTO VALLARTA</p>
+    <img src="/imgNomadas/PlayaVallarta.jpg" alt="" class=" w-16 sm:w-44 h-14 sm:h-32 absolute top-2 left-2" />
+    <p class= "absolute bottom-4 sm:bottom-8 left-0 text-[5px] sm:text-[10px] text-orange-500 px-2 py-1 text-center pr-2 ">Puedo llevarte a conocer la refrescante y tropical costa de puerto vallarta como local </p>
+    <button
+         class="absolute none top-28 sm:top-52 left-6 sm:left-[60px] rounded-lg bg-gradient-to-tr from-red-500 to-orange-500 py-1 sm:py-2 px-2 sm:px-4 font-sans text-[4px] sm:text-[7px] font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+         Mas info
+         </button> 
+    </div>
+    <div class="absolute bottom-6 left-4 sm:left-[30px] w-20 sm:w-48 h-32 sm:h-60  bg-amber-100">
+    <p class= "absolute bottom-12 sm:bottom-20 left-2 sm:left-[28px] text-[4px] sm:text-[10px] text-orange-500 px-2 py-1 text-center pr-2 font-bold ">TONALA RUTA ALFARERA</p>
+    <img src="/imgNomadas/Tonala.png" alt="" class=" w-16 sm:w-44 h-14 sm:h-32 absolute top-2 left-2" />
+    <p class= "absolute bottom-4 sm:bottom-8 left-0 text-[5px] sm:text-[10px] text-orange-500 px-2 py-1 text-center pr-2 ">Demos una vuelta por tonala, y conozcamos su tianguis de artesanias y porque es la capital alfarera </p>
+    <button
+         class="absolute none top-28 sm:top-52 left-6 sm:left-[60px] rounded-lg bg-gradient-to-tr from-red-500 to-orange-500 py-1 sm:py-2 px-2 sm:px-4 font-sans text-[4px] sm:text-[7px] font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+         Mas info
+         </button> 
+    </div>
+    </div>
+    
+    
+    </div>
+
+    
+
+     
+
+
+        </SideBar>
+
+        </>
+    )
+}
